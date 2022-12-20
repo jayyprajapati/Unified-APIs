2118b57d815025c68204d0469e11b56ed9aaad83
jayyprajapati <jay.prajapati5717@gmail.com>
2022-12-20 22:16:01 +0530

    remove console logs

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const mongoUserName = process.env.REACT_APP_MONGO_USERNAME;
 const mongoUserPassword = process.env.REACT_APP_MONGO_USER_PASSWORD;
-console.log(mongoUserName);
-console.log(mongoUserPassword);
 
 const mongoURI = `mongodb+srv://${mongoUserName}:${mongoUserPassword}@cloudnotes.bx9dv3c.mongodb.net/?retryWrites=true&w=majority`;
 
